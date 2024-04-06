c5e4f85520b37a73a71c08afe400c9c9d4eb440e
Siva70136 <cheerlasivamallikharjuna@.com>
2024-04-06 09:49:31 +0530

    implemented api

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+// Generate random grid pattern
+
+const generateGridPattern = () => {
+    const colors = ['blue', 'red'];
+    const grid = [];
+
+    for (let i = 0; i < 10; i++) {
+        const row = [];
+        for (let j = 0; j < 10; j++) {
+            const randomIndex = Math.floor(Math.random() * colors.length);
+            console.log(randomIndex)
+            row.push(colors[randomIndex]);
+        }
+        grid.push(row);
+    }
+
+    return grid;
+};
+
+let currentGrid;
+
+app.get('/grid-pattern', (req, res) => {
+    currentGrid = generateGridPattern();
+    res.json({ grid: currentGrid });
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
